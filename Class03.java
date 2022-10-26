e9321f565e61a33298e3239021a58df524ee47d6
chu1111 <113088657+chu1111@users.noreply.github.com>
2022-10-26 09:20:59 +0800

    Add files via upload

---
+++ Class03.java
@@ -0,0 +1,36 @@
+package Java11;
+import static java.lang.System.out;
+public class Class03 {
+	public static void main(String args[]) {
+		Compute cmp = new Compute();
+		cmp.mul(3,  5);
+		cmp.show();
+		// Math mth = new Math(); 抽象類別無法被實體化
+	}
+}
+
+abstract class Math{
+	protected int ans;
+	public void show() {
+		System.out.println("ans="+ans);
+	}
+	public abstract void add(int a, int b);
+	public abstract void sub(int a, int b);
+	public abstract void mul(int a, int b);
+	public abstract void div(int a, int b);
+}
+
+class Compute extends Math{
+	public void add(int a, int b) {
+		ans = a + b;
+	}
+	public void sub(int a, int b) {
+		ans = a - b;
+	}
+	public void mul(int a, int b) {
+		ans = a * b;
+	}
+	public void div(int a, int b) {
+		ans = a / b;
+	}
+}
